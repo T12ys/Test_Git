882e4642238668c4d07b65c943ee56a2ed3e51fc
T12ys <amil.m0508@gmail.com>
2025-03-08 02:10:26 +0400

    -Homework first branch

--- Git_Test/Git_Test/Git_Test.cpp
+++ Git_Test/Git_Test/Git_Test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-    std::cout << "Hello World!\n";
+void featureOne() {
+    std::cout << "Feature One is active!" << std::endl;
+}
+
+int main() {
+    std::cout << "Hello from main branch!" << std::endl;
+    featureOne();
     return 0;
 }
